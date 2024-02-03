4b4fbfb1f55d428eae7f56ff12c9e3d7324e9bdc
suryamageshk <83328984+suryamageshk@users.noreply.github.com>
2024-02-03 21:52:51 +0530

    Create Olympic Gold Medals Problem.sql

---
+++ Olympic Gold Medals Problem.sql
@@ -0,0 +1,30 @@
+-- Script for Table Creation:
+CREATE TABLE events (
+    id INT,
+    event VARCHAR(255),
+    year INT,
+    gold VARCHAR(255),
+    silver VARCHAR(255),
+    bronze VARCHAR(255)
+);
+
+-- Initial Table Delete
+DELETE FROM events;
+
+-- Insert Statements
+INSERT INTO events VALUES (1,'100m',2016, 'Amthhew Mcgarray','donald','barbara');
+INSERT INTO events VALUES (2,'200m',2016, 'Nichole','Alvaro Eaton','janet Smith');
+INSERT INTO events VALUES (3,'500m',2016, 'Charles','Nichole','Susana');
+INSERT INTO events VALUES (4,'100m',2016, 'Ronald','maria','paula');
+INSERT INTO events VALUES (5,'200m',2016, 'Alfred','carol','Steven');
+INSERT INTO events VALUES (6,'500m',2016, 'Nichole','Alfred','Brandon');
+INSERT INTO events VALUES (7,'100m',2016, 'Charles','Dennis','Susana');
+INSERT INTO events VALUES (8,'200m',2016, 'Thomas','Dawn','catherine');
+INSERT INTO events VALUES (9,'500m',2016, 'Thomas','Dennis','paula');
+INSERT INTO events VALUES (10,'100m',2016, 'Charles','Dennis','Susana');
+INSERT INTO events VALUES (11,'200m',2016, 'jessica','Donald','Stefeney');
+INSERT INTO events VALUES (12,'500m',2016,'Thomas','Steven','Catherine');
+
+COMMIT;
+
+SELECT * FROM events;
