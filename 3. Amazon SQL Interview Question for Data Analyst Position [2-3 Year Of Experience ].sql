c9ee685c0a44f50512c69b6a816a22e0d1e67820
suryamageshk <83328984+suryamageshk@users.noreply.github.com>
2024-02-07 19:38:52 +0530

    Update 3. Amazon SQL Interview Question for Data Analyst Position [2-3 Year Of Experience ].sql

--- 3. Amazon SQL Interview Question for Data Analyst Position [2-3 Year Of Experience ].sql
+++ 3. Amazon SQL Interview Question for Data Analyst Position [2-3 Year Of Experience ].sql
@@ -1,21 +1,22 @@
 -- Script:
-create table hospital (
-  emp_id int, 
-  action varchar(10), 
-  time datetime
+CREATE TABLE hospital (
+  emp_id INT, 
+  action VARCHAR(10), 
+  time DATETIME
 );
 
-insert into hospital values ('1', 'in', '2019-12-22 09:00:00');
-insert into hospital values ('1', 'out', '2019-12-22 09:15:00');
-insert into hospital values ('2', 'in', '2019-12-22 09:00:00');
-insert into hospital values ('2', 'out', '2019-12-22 09:15:00');
-insert into hospital values ('2', 'in', '2019-12-22 09:30:00');
-insert into hospital values ('3', 'out', '2019-12-22 09:00:00');
-insert into hospital values ('3', 'in', '2019-12-22 09:15:00');
-insert into hospital values ('3', 'out', '2019-12-22 09:30:00');
-insert into hospital values ('3', 'in', '2019-12-22 09:45:00');
-insert into hospital values ('4', 'in', '2019-12-22 09:45:00');
-insert into hospital values ('5', 'out', '2019-12-22 09:40:00');
+INSERT INTO hospital VALUES 
+('1', 'in', '2019-12-22 09:00:00')
+,('1', 'out', '2019-12-22 09:15:00')
+,('2', 'in', '2019-12-22 09:00:00')
+,('2', 'out', '2019-12-22 09:15:00')
+,('2', 'in', '2019-12-22 09:30:00')
+,('3', 'out', '2019-12-22 09:00:00')
+,('3', 'in', '2019-12-22 09:15:00')
+,('3', 'out', '2019-12-22 09:30:00')
+,('3', 'in', '2019-12-22 09:45:00')
+,('4', 'in', '2019-12-22 09:45:00')
+,('5', 'out', '2019-12-22 09:40:00')
 
 SELECT * FROM hospital;
 
